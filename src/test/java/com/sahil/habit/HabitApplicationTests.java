826a5a57da4edc82c6ed125152d119b604ad46c9
SahilRaj1 <raj24sahil@gmail.com>
2025-03-23 00:28:23 +0530

    [phase 1] project init

---
+++ src/test/java/com/sahil/habit/HabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.habit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
